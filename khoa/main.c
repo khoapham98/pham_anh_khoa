54cdcbf7353f262fc6f4b1ddde4396f795aca544
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-07 16:40:47 +0700

    ATM

--- khoa/main.c
+++ khoa/main.c
@@ -2,48 +2,60 @@
 
 int main()
 {
-	int x;
-	printf("Enter your income: ");
-	scanf_s("%d", &x);
+	short password = 1234;
+	int cnt = 0, step = 1;
 
-	int tax, income = x - 11000000;
-
-	if (income <= 0)
+	while (cnt < 3)
 	{
-		tax = 0;
-	}
-	else
-	{
-		if (income <= 5e6)
+		switch (step)
 		{
-			tax = income * 0.05;
-		}
-		else if (income <= 10e6)
-		{
-			tax = (5e6 * 0.05) + ((income - 5e6) * 0.1);
-		}
-		else if (income <= 18e6)
-		{
-			tax = (5e6 * 0.05) + (5e6 * 0.1) + ((income - 10e6) * 0.15);
-		}
-		else if (income <= 32e6)
-		{
-			tax = (5e6 * 0.05) + (5e6 * 0.1) + (8e6 * 0.15) + ((income - 18e6) * 0.2);
-		}
-		else if (income <= 52e6)
-		{
-			tax = (5e6 * 0.05) + (5e6 * 0.1) + (8e6 * 0.15) + (14e6 * 0.2) + ((income - 32e6) * 0.25);
-		}
-		else if (income <= 80e6)
-		{
-			tax = (5e6 * 0.05) + (5e6 * 0.1) + (8e6 * 0.15) + (14e6 * 0.2) + (20e6 * 0.25) + ((income - 52e6) * 0.3);
-		}
-		else
-		{
-			tax = (5e6 * 0.05) + (5e6 * 0.1) + (8e6 * 0.15) + (14e6 * 0.2) + (20e6 * 0.25) + (28e6 * 0.3) + ((income - 80e6) * 0.35);
+		case 1:
+			int userEnter;
+			printf("Enter your password: ");
+			scanf_s("%d", &userEnter);
+			if (userEnter == password)
+			{
+				step = 2;
+			}
+			else
+			{
+				cnt++;
+				step = 1;
+			}
+			break;
+		case 2: 
+			int amount;
+			printf("Enter the amount of money you want to withdraw: ");
+			scanf_s("%d", &amount);
+			if (amount > 10000)
+			{
+				step = 2;
+			}
+			else
+			{
+				printf("$ = %d VND\n", amount);
+				step = 3;
+			}
+			break;
+		case 3: 
+			short choice; 
+			printf("Continue ? (1-Y / 0-N) : ");
+			scanf_s("%hi", &choice);
+			if (choice == 0)
+			{
+				step = 4;
+			}
+			else
+			{
+				step = 2;
+			}
+			break;
+		case 4:
+			return 0;
+		default:
+			break;
 		}
 	}
 
-	printf("Your taxable income : %d VND", tax);
 	return 0;
 }
