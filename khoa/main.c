98084dbb6d5905616a29ea4ad5b94a8154585811
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-07 14:55:38 +0700

    bài 7 - v2

--- khoa/main.c
+++ khoa/main.c
@@ -6,33 +6,74 @@ int main()
 	printf("KWh : ");
 	scanf_s("%d", &kwh);
 
-	int money = 1;
-	short x = 50, y = 100;
-	if (kwh <= 50)
+	int money = 0;
+	int i = 0;
+	while (kwh > 0)
 	{
-		money = 1678 * kwh;
-	}
-	else if (kwh <= 100)
-	{
-		money = (1678 * x) + (1734 * (kwh - 50));
-	}
-	else if (kwh <= 200)
-	{
-		money = (1678 * x) + (1734 * x) + (2014 * (kwh - 100));
-	}
-	else if (kwh <= 300)
-	{
-		money = (1678 * x) + (1734 * x) + (2014 * y) + (2536 * (kwh - 200));
-	}
-	else if (kwh <= 400)
-	{
-		money = (1678 * x) + (1734 * x) + (2014 * y) + (2536 * y) + (2834 * (kwh - 300));
-	}
-	else
-	{
-		money = (1678 * x) + (1734 * x) + (2014 * y) + (2536 * y) + (2834 * y) + (2927 * (kwh - 400));
+		++i;
+		switch (i)
+		{
+		case 1:
+			if (kwh < 50)
+			{
+				money = kwh * 1678;
+			}
+			else
+			{
+				money = 50 * 1678;
+			}
+			kwh -= 50;
+			break;
+		case 2:
+			if (kwh < 50)
+			{
+				money += (kwh * 1734);
+			}
+			else
+			{
+				money += (50 * 1734);
+			}
+			kwh -= 50;
+			break;
+		case 3:
+			if (kwh < 100)
+			{
+				money += (kwh * 2014);
+			}
+			else
+			{
+				money += (100 * 2014);
+			}
+			kwh -= 100;
+			break;
+		case 4:
+			if (kwh < 100)
+			{
+				money += (kwh * 2536);
+			}
+			else
+			{
+				money += (100 * 2536);
+			}
+			kwh -= 100;
+			break;
+		case 5:
+			if (kwh < 100)
+			{
+				money += (kwh * 2834);
+			}
+			else
+			{
+				money += (100 * 2834);
+			}
+			kwh -= 100;
+			break;
+		default:
+			money += (kwh * 2927);
+			kwh = 0;
+		}
 	}
 
-	printf("You have to pay : %d VND", money);
+	printf("amount = %d VND", money);
 	return 0;
 }
