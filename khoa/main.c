f8e810aebb0ce41a0d097c1521eb97437100aed6
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-07 14:29:00 +0700

    bài 7 - v1

--- khoa/main.c
+++ khoa/main.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
-#include <math.h>
+
 int main()
 {
-	float gpa;
-	printf("Enter student's GPA: ");
-	scanf_s("%f", &gpa);
+	int kwh;
+	printf("KWh : ");
+	scanf_s("%d", &kwh);
 
-	if (gpa >= 8)
+	int money = 1;
+	short x = 50, y = 100;
+	if (kwh <= 50)
 	{
-		printf("GIOI\n");
+		money = 1678 * kwh;
 	}
-	else if (gpa >= 6.5)
+	else if (kwh <= 100)
 	{
-		printf("KHA\n");
+		money = (1678 * x) + (1734 * (kwh - 50));
 	}
-	else if (gpa >= 5.0)
+	else if (kwh <= 200)
 	{
-		printf("TRUNG BINH\n");
+		money = (1678 * x) + (1734 * x) + (2014 * (kwh - 100));
+	}
+	else if (kwh <= 300)
+	{
+		money = (1678 * x) + (1734 * x) + (2014 * y) + (2536 * (kwh - 200));
+	}
+	else if (kwh <= 400)
+	{
+		money = (1678 * x) + (1734 * x) + (2014 * y) + (2536 * y) + (2834 * (kwh - 300));
 	}
 	else
 	{
-		printf("YEU\n");
+		money = (1678 * x) + (1734 * x) + (2014 * y) + (2536 * y) + (2834 * y) + (2927 * (kwh - 400));
 	}
 
+	printf("You have to pay : %d VND", money);
 	return 0;
 }
