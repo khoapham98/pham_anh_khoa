7cec72c07cfc01e35ce3df170c3b559e9b36cedc
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-11 19:55:58 +0700

    bang cuu chuong

--- khoa/main.c
+++ khoa/main.c
@@ -2,59 +2,13 @@
 
 int main()
 {
-	short password = 1234;
-	int cnt = 0, step = 1;
-
-	while (cnt < 3)
+	for (int i = 1; i <= 10; i++)
 	{
-		switch (step)
+		for (int j = 2; j < 10; j++)
 		{
-		case 1:
-			int userEnter;
-			printf("Enter your password: ");
-			scanf_s("%d", &userEnter);
-			if (userEnter == password)
-			{
-				step = 2;
-			}
-			else
-			{
-				cnt++;
-				step = 1;
-			}
-			break;
-		case 2: 
-			int amount;
-			printf("Enter the amount of money you want to withdraw: ");
-			scanf_s("%d", &amount);
-			if (amount > 10000)
-			{
-				step = 2;
-			}
-			else
-			{
-				printf("$ = %d VND\n", amount);
-				step = 3;
-			}
-			break;
-		case 3: 
-			short choice; 
-			printf("Continue ? (1-Y / 0-N) : ");
-			scanf_s("%hi", &choice);
-			if (choice == 0)
-			{
-				step = 4;
-			}
-			else
-			{
-				step = 2;
-			}
-			break;
-		case 4:
-			return 0;
-		default:
-			break;
+			printf("%d x %d = %d \t", j, i, i * j);
 		}
+		printf("\n");
 	}
 
 	return 0;
