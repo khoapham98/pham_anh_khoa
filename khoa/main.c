27321c095584b5997343c06eceb5604ccfff23f5
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-12 20:51:48 +0700

    bai 4

--- khoa/main.c
+++ khoa/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-
+#include <math.h>
 int main()
 {
-	int x, i = 0; 
-	do
-	{
-		printf("Nhap so : ");
-		scanf_s("%d", &x);
-		i++;
-	} while (x != 0 && i < 5);
+	int money;
+	printf("Amount of money you want to deposit (VND) : ");
+	scanf_s("%d", &money);
+	float interest;
+	printf("Percent of interest per year : ");
+	scanf_s("%f", &interest);
+	int year;
+	printf("Number of years : ");
+	scanf_s("%d", &year);
+
+	long long res;
+	res = money * pow((1 + (interest / 100)), year);
+	printf("$ after %d years : %lld VND\n", year, res);
 
 	return 0;
 }
