25a09460dff9fc5d731a215e49e57237205d49b2
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-12 20:32:02 +0700

    bai 3

--- khoa/main.c
+++ khoa/main.c
@@ -3,15 +3,11 @@
 int main()
 {
 	int x; 
-	printf("Nhap so : ");
-	scanf_s("%d", &x);
-
-	long long res = 1;
-	for (int i = 1; i <= x; i++)
+	do
 	{
-		res *= i;
-	}
+		printf("Nhap so : ");
+		scanf_s("%d", &x);
+	} while (x != 0);
 
-	printf("Giai thua cua %d la : %lld", x, res);
 	return 0;
 }
