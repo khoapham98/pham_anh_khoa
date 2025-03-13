5babd2dda6a2a7e3b49c05c139cc0f073237e7f6
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-13 15:54:04 +0700

    bai 9

--- khoa/main.c
+++ khoa/main.c
@@ -3,36 +3,36 @@
 
 int main()
 {
-	int income;
-	printf("Your monthly income (kVND) : ");
-	scanf_s("%d", &income);
-
-	float percent[] = { 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35 };
-	int level[] = { 5000, 5000, 8000, 14000, 20000, 28000 };
-
-	int tax = income - 11000;
-	if (tax <= 0)
+	int enterMoney;
+	do
 	{
-		printf("No taxes! Congrats!");
-	}
-	else
+		printf("Amount of money you want to withdraw (kVND) : ");
+		scanf_s("%d", &enterMoney);
+	} while (enterMoney % 50 != 0);
+
+	int price[] = { 50, 100, 200, 500 };
+	int to[] = { 0, 0, 0, 0 };
+	int moneyOut = 0;
+	int i = 0;
+
+	while (moneyOut < enterMoney)
 	{
-		float pay = 0;
-		int i = 0;
-		while (tax > 0 && i < 7)
+		if ((moneyOut + price[i]) <= enterMoney)
 		{
-			if (tax <= level[i] || i == 6)
-			{
-				pay += tax * percent[i];
-			}
-			else
-			{
-				pay += level[i] * percent[i];
-			}
-			tax -= level[i];
-			i++;
+			moneyOut += price[i];
+			to[i]++;
 		}
-		printf("Your tax is : %.0f kVND \n", pay);
+		i++;
+
+		if (i > 3)
+		{
+			i = 0;
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		printf("%d x %dk VND \n", to[i], price[i]);
 	}
 
 	return 0;
