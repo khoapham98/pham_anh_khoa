cc95345fb378572dc0c8df32436599bbe6be700e
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-13 20:09:28 +0700

    find max

--- khoa/main.c
+++ khoa/main.c
@@ -9,9 +9,25 @@ int main()
 	for (int i = 0; i < 10; i++)
 	{
 		scanf_s("%d", &arr[i]);
-		res += arr[i];
 	}
-	printf("Sum = %d\nSize = %llu byte", res, sizeof(arr));
+
+	int max = arr[0];
+	for (int i = 1; i < 10; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+
+	printf("Positions of max = %d is : ", max);
+	for (int i = 0; i < 10; i++)
+	{
+		if (arr[i] == max)
+		{
+			printf("%d ", i);
+		}
+	}
 
 	return 0;
 }
