dff64042e50e736eefb3fdb30f156d2f03acaf82
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-17 22:30:55 +0700

    string - bai 5

--- khoa/main.c
+++ khoa/main.c
@@ -1,24 +1,27 @@
 ﻿#include <stdio.h>
-#include <math.h>
 
-int WordCnt(char str[]);
+int CapFirstLetter(char str[]);
 int isWord(char ch);
 
 int main()
 {
-	char string[] = "Hello\tworld\nthis is\ta test";
-	printf("Number of word : %d", WordCnt(string));
+	char string[] = "nguyen van a";
+	
+	CapFirstLetter(string);
+
+	printf("New string : %s", string);
+	
 	return 0;
 }
 
-int WordCnt(char str[])
+int CapFirstLetter(char str[])
 {
 	int cnt = 0, i = 0;
 	while (str[i] != 0)
 	{
-		if (isWord(str[i]) && !isWord(str[i + 1]))
+		if (isWord(str[i]) && str[i] >= 97 && !isWord(str[i - 1]))
 		{
-			cnt++;
+			str[i] -= 32;
 		}
 		i++;
 	}
