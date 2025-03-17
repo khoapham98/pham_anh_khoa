84f0a991c92a03a2a0420a7d6a99ba47883afdbb
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-17 20:12:26 +0700

    array - bai 2

--- khoa/main.c
+++ khoa/main.c
@@ -4,19 +4,26 @@
 int main()
 {
 	int arr[10] = { 0 };
-	
+	int sum = 0, cnt = 0;
+
 	for (int i = 0; i < 10; i++)
 	{
 		printf("arr[%d] = ", i);
 		scanf_s("%d", &arr[i]);
+		if (arr[i] % 2 != 0)
+		{
+			sum += arr[i];
+			cnt++;
+		}
 	}
-	
-	int sum = 0;
-	for (int i = 0; i < 10; i++)
-	{
-		sum += arr[i];
-	}
-	printf("SUM = %d", sum);
 
+	if (sum == 0)
+	{
+		printf("Khong co so le trong mang!");
+	}
+	else
+	{
+		printf("SUM = %.2f", sum / (float) cnt);
+	}
 	return 0;
 }
