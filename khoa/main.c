d34404f57ae839353bdeb2c905c95717760b094a
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-17 22:20:22 +0700

    string - bai 4

--- khoa/main.c
+++ khoa/main.c
@@ -1,25 +1,39 @@
 ﻿#include <stdio.h>
 #include <math.h>
 
-void Capital(char str[]);
+int WordCnt(char str[]);
+int isWord(char ch);
 
 int main()
 {
-	char string[] = "Hello World";
-	Capital(string);
-	printf("Number of spaces : %s", string);
+	char string[] = "Hello\tworld\nthis is\ta test";
+	printf("Number of word : %d", WordCnt(string));
 	return 0;
 }
 
-void Capital(char str[])
+int WordCnt(char str[])
 {
 	int cnt = 0, i = 0;
 	while (str[i] != 0)
 	{
-		if (str[i] >= 97 && str[i] <= 122)
+		if (isWord(str[i]) && !isWord(str[i + 1]))
 		{
-			str[i] -= 32;
+			cnt++;
 		}
 		i++;
+	}
+
+	return cnt;
+}
+
+int isWord(char ch)
+{
+	if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+	{
+		return 1; 
+	}
+	else
+	{
+		return 0;
 	}
 }
