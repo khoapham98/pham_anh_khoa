c4032a901374cb2b2bc2ddc357a53a9201b472fb
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-22 14:47:12 +0700

    bài 1 : đếm số ký tự trong chuỗi

--- khoa/main.c
+++ khoa/main.c
@@ -4,18 +4,8 @@
 int main()
 {
 	char str[] = "I Love c programming";
-	char substr[] = "programming";
 
+	printf("Number of characters in string : %d", findSize(str));
 
-
-	int i = 0;
-	while (*(str + i) != 0)
-	{
-		printf("%c = %p\n",*(str + i), str + i);
-		i++;
-	}
-	char* C_address = findCh(str, 'c');
-	printf("Address of 'c' : %p", C_address);
-	//char* sub_address = findSubstr(str, substr);
 	return 0;
 }
