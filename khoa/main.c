19de915acb9da12c9f6b071e224589170965cfb6
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-25 20:28:13 +0700

    kiểu dữ liệu học sinh

--- khoa/main.c
+++ khoa/main.c
@@ -5,44 +5,19 @@
 
 typedef struct
 {
-	int tu;
-	int mau;
-} phan_so;
+	char* ten;
+	int tuoi;
+	float diem_toan;
+	float diem_van;
+	float gpa;
+	int xep_loai;
+} hocsinh;
 
-phan_so nhan_2_phan_so(phan_so a, phan_so b)
-{
-	phan_so res;
-	res.tu = a.tu * b.tu;
-	res.mau = a.mau * b.mau;
-
-	return res;
-}
-
-phan_so findMax(phan_so* ptr, int size)
-{
-	float max = ptr->tu / ptr->mau;
-	int index = 0;
-
-	for (int i = 1; i < size; i++)
-	{
-		float tmp = ptr[i].tu / (float) ptr[i].mau;
-		if (tmp > max)
-		{
-			max = tmp;
-			index = i;
-		}
-	}
-	phan_so res = ptr[index];
-
-	return res;
-}
 
 int main()
 {
-	phan_so arr[] = { {1,2}, {2,3}, {3,4}, {4,5}, {5,6} };
+	hocsinh a = { "anh khoa", 22, 7.5, 4.6, 5.4, 1 };
 
-	phan_so kq = findMax(arr, 5);
-	printf("Phan so lon nhat: %d/%d", kq.tu, kq.mau);
-
+	printf("%s", a.ten);
 	return 0;
 }
