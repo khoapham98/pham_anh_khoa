9e05eb66015b548788ea645e42e880868c804e04
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-26 21:08:01 +0700

    viết hàm in ra màn hình danh sách n học sinh

--- khoa/main.c
+++ khoa/main.c
@@ -13,11 +13,15 @@ typedef struct
 } student;
 
 void enterInfo(student* list, int size);
+void printList(student* list, int size);
 
 int main()
 {
 	student studentList[4] = { 0 };
-	enterInfo(studentList, 2);
+	
+	enterInfo(studentList, 4);
+
+	printList(studentList, 4);
 
 	return 0;
 }
@@ -39,5 +43,39 @@ void enterInfo(student* list, int size)
 		printf("Literature Score: ");
 		scanf("%f", &list[i].literScore);
 		getchar();
+	}
+}
+
+
+void printList(student* list, int size)
+{
+	printf("Student List:\n");
+	for (int i = 0; i < size; i++)
+	{
+		list[i].GPA = (list[i].mathScore + list[i].literScore) / 2;
+		if (list[i].GPA >= 8)
+		{
+			list[i].rank = 0;
+		}
+		else if (list[i].GPA >= 6.5)
+		{
+			list[i].rank = 1;
+		}
+		else if (list[i].GPA >= 5)
+		{
+			list[i].rank = 2;
+		}
+		else
+		{
+			list[i].rank = 3;
+		}
+
+		printf("%d. Name: %s, ", i + 1, list[i].name);
+		printf("age: %d, ", list[i].age);
+		printf("sex: %s, ", list[i].sex);
+		printf("math score: %.2f, ", list[i].mathScore);
+		printf("literature score: %.2f, ", list[i].literScore);
+		printf("GPA: %.2f, ", list[i].GPA);
+		printf("Rank: %d\n", list[i].rank);
 	}
 }
