6bee738c83d5c8f77c61fab507a12428d57ff16f
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-26 10:16:22 +0700

    viết hàm cộng, trừ, nhân, chia 2 phân số

--- khoa/main.c
+++ khoa/main.c
@@ -5,72 +5,54 @@
 
 typedef struct
 {
-	char* ten;
-	int tuoi;
-	float diem_toan;
-	float diem_van;
-	float gpa;
-	int xep_loai;
-} hocsinh;
+	int tu;
+	int mau;
+} phanSo;
 
-void tinhdiem(hocsinh* ptr, int size)
+phanSo congPhanSo(phanSo x, phanSo y)
 {
-	for (int i = 0; i < size; i++)
-	{
-		ptr[i].gpa = (ptr[i].diem_toan + ptr[i].diem_van) / 2;
-		if (ptr[i].gpa >= 8)
-		{
-			ptr[i].xep_loai = 0;
-		}
-		else if (ptr[i].gpa >= 6.5)
-		{
-			ptr[i].xep_loai = 1;
-		}
-		else if (ptr[i].gpa >= 5)
-		{
-			ptr[i].xep_loai = 2;
-		}
-		else
-		{
-			ptr[i].xep_loai = 3;
-		}
-	}
+	phanSo res;
+	res.tu = x.tu + y.tu;
+	res.mau = x.mau + y.mau;
+
+	return res;
 }
 
-hocsinh timHSG(hocsinh* ptr, int size)
+phanSo truPhanSo(phanSo x, phanSo y)
 {
-	float max = ptr[0].gpa;
-	int index = 0;
+	phanSo res;
+	res.tu = x.tu - y.tu;
+	res.mau = x.mau - y.mau;
 
-	for (int i = 1; i < size; i++)
-	{
-		if (ptr[i].gpa > max)
-		{
-			max = ptr[i].gpa;
-			index = i;
-		}
-	}
+	return res;
+}
 
-	return ptr[index];
+phanSo nhanPhanSo(phanSo x, phanSo y)
+{
+	phanSo res;
+	res.tu = x.tu * y.tu;
+	res.mau = x.mau * y.mau;
+
+	return res;
+}
+
+phanSo chiaPhanSo(phanSo x, phanSo y)
+{
+	phanSo res;
+	res.tu = x.tu * y.mau;
+	res.mau = x.mau * y.tu;
+
+	return res;
 }
 
 int main()
 {
-	hocsinh danhSachHocsinh[] = {
-		{ "anh khoa", 22, 7.5, 6.5 },
-		{ "quoc qui", 22, 8, 7},
-		{ "quy hoa", 22, 8.5, 7.5},
-		{ "tommy", 22, 5, 4.5},
-	};
+	phanSo a = { 1, 3 }, b = { 4, 5 };
 
-	tinhdiem(danhSachHocsinh, 4);
-
-	for (int i = 0; i < 4; i++)
-	{
-		printf("xep loai cua %s : %d - GPA : %.2f \n", danhSachHocsinh[i].ten, danhSachHocsinh[i].xep_loai, danhSachHocsinh[i].gpa);
-	}
-
-	hocsinh a = timHSG(danhSachHocsinh, 4);
-	printf("Hoc sinh co GPA cao nhat: %s", a.ten);
+	printf("Tong: %d/%d\n", congPhanSo(a, b).tu, congPhanSo(a, b).mau);
+	printf("Hieu: %d/%d\n", truPhanSo(a, b).tu, truPhanSo(a, b).mau);
+	printf("Tich: %d/%d\n", nhanPhanSo(a, b).tu, nhanPhanSo(a, b).mau);
+	printf("Thuong: %d/%d\n", chiaPhanSo(a, b).tu, chiaPhanSo(a, b).mau);
+	
 	return 0;
 }
