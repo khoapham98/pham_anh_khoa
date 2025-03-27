15ac47f4d9c997521aa552396bfbfed990477541
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-27 14:47:29 +0700

    update

--- khoa/linked_list.c
+++ khoa/linked_list.c
@@ -1,5 +1,6 @@
 #include "linked_list.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 node_t* createNode(int x)
 {
--- khoa/linked_list.h
+++ khoa/linked_list.h
@@ -10,3 +10,5 @@ typedef struct
 	node_t* last_node;
 	int len;
 }linked_list_t;
+
+node_t* createNode(int x);
