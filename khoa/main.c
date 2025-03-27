6108f0a6f42bafc6717e29be3db42659b240497b
anhkh04 <phamanhkhoa9803@gmail.com>
2025-03-27 20:20:29 +0700

    thêm hàm xử lý chuỗi

--- khoa/main.c
+++ khoa/main.c
@@ -1,17 +1,41 @@
 ﻿#include <stdio.h>
-#include "linked_list.h"
+#include <string.h>
+#include <stdlib.h>
+
+int xyluChuoi(char* str);
 
 int main()
 {
-	linked_list_t list;
+	char* str = "led_1: on, led_2: off, led_3: on";
 
-	createLinklist(&list);
-
-	addNode(&list, 10);
-	addNode(&list, 40);
-	addNode(&list, 70);
-
-	printf("number of nodes :  %d", getlen(&list));
+	printf("%d", xyluChuoi(str));
 
 	return 0;
+}
+
+int xyluChuoi(char* str)
+{
+	char* substr = "led_2: ";
+	char* ptr = strstr(str, substr) + strlen(substr);
+
+	int size = 0;
+	while (ptr[size] != ',')
+	{
+		size++;
+	}
+
+	char stt[3];
+	for (int i = 0; i < size; i++)
+	{
+		stt[i] = ptr[i];
+	}
+
+	if (!strcmp(stt, "on"))
+	{
+		return 1;
+	}
+	else if (!strcmp(stt, "off"))
+	{
+		return 0;
+	}
 }
