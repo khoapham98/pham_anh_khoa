724c83d606fa4c8ece971d983c9f9f16295639b5
anhkh04 <phamanhkhoa9803@gmail.com>
2025-04-29 21:48:33 +0700

    tạo pipe

--- khoa/main.c
+++ khoa/main.c
@@ -3,37 +3,22 @@
 #include <Windows.h>
 #include <string.h>
 
-HANDLE mutex_lock;
-char cnt;
+volatile char cnt;
+HANDLE readPipe, writePipe; 
 
-void printf_log(char* s)
-{
-	for (int i = 0; i < strlen(s); i++)
-	{
-		printf("%c", s[i]);
-		Sleep(300);
-	}
-}
 
 DWORD WINAPI myThread(LPVOID lpParameter)
 {
-	while (1)
-	{
-		WaitForSingleObject(mutex_lock, INFINITE);
-		printf_log("This is myThread function!\n");
-		ReleaseMutex(mutex_lock);
-	}
+
 }
 
 int main()
 {
 	HANDLE thread = CreateThread(NULL, 0, myThread, NULL, 0, NULL);
-	mutex_lock = CreateMutexA(NULL, 0, NULL);
-	while (1)
+	if (CreatePipe(&readPipe, &writePipe, NULL, 0))
 	{
-		WaitForSingleObject(mutex_lock, INFINITE);
-		printf_log("This is main function!\n");
-		ReleaseMutex(mutex_lock);
+		printf("Tao pipe thanh cong!");
 	}
+
 	return 0;
 }
