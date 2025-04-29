eb13c2645c6a5d5b04677ffdb5560bfb117989b7
anhkh04 <phamanhkhoa9803@gmail.com>
2025-04-29 22:48:06 +0700

    đọc dữ liệu từ pipe

--- khoa/main.c
+++ khoa/main.c
@@ -3,26 +3,43 @@
 #include <Windows.h>
 #include <string.h>
 
-volatile char cnt;
 HANDLE readPipe, writePipe; 
-
+HANDLE mutex; 
 
 DWORD WINAPI myThread(LPVOID lpParameter)
 {
-
+	WaitForSingleObject(mutex, INFINITE);
+	int data2R;
+	printf("Du lieu trong pipe: ");
+	while (1)
+	{
+		DWORD bytesRead; 
+		if (ReadFile(readPipe, &data2R, sizeof(data2R), &bytesRead, NULL))
+		{
+			printf("%d ", data2R);
+		}
+		else
+		{
+			printf("Khong the doc du lieu trong pipe!\n");
+			return 0;
+		}
+	}
+	printf("Doc du lieu thanh cong!\n");
+	
+	ReleaseMutex(mutex);
 }
 
 int main()
 {
 	HANDLE thread = CreateThread(NULL, 0, myThread, NULL, 0, NULL);
+	mutex = CreateMutexA(NULL, 1, NULL);
 
 	if (CreatePipe(&readPipe, &writePipe, NULL, 0))
 	{
 		printf("Tao pipe thanh cong!\r\n");
 	}
 	
-	int data[] = { 0, 1, 4, 6, 3, 7, 3, 7, 2, 9 };
-
+	int data[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	for (int i = 0; i < 10; i++)
 	{
 		DWORD bytesWritten; 
@@ -33,5 +50,6 @@ int main()
 		}
 	}
 	printf("Ghi du lieu vao pipe thanh cong!\n");
+	ReleaseMutex(mutex);
 	return 0;
 }
