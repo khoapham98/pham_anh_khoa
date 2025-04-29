4d8dc7fc145e2a213bd10435c085dddf1e09f8c4
anhkh04 <phamanhkhoa9803@gmail.com>
2025-04-29 22:16:26 +0700

    ghi dữ liệu vào pipe

--- khoa/main.c
+++ khoa/main.c
@@ -15,10 +15,23 @@ DWORD WINAPI myThread(LPVOID lpParameter)
 int main()
 {
 	HANDLE thread = CreateThread(NULL, 0, myThread, NULL, 0, NULL);
+
 	if (CreatePipe(&readPipe, &writePipe, NULL, 0))
 	{
-		printf("Tao pipe thanh cong!");
+		printf("Tao pipe thanh cong!\r\n");
 	}
+	
+	int data[] = { 0, 1, 4, 6, 3, 7, 3, 7, 2, 9 };
 
+	for (int i = 0; i < 10; i++)
+	{
+		DWORD bytesWritten; 
+		if (!WriteFile(writePipe, &data[i], sizeof(data[i]), &bytesWritten, NULL))
+		{
+			printf("Ghi du lieu vao pipe that bai!\n");
+			break;
+		}
+	}
+	printf("Ghi du lieu vao pipe thanh cong!\n");
 	return 0;
 }
