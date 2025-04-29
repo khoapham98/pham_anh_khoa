27ef113701897fb34ec786f1236e1f6f784db524
anhkh04 <phamanhkhoa9803@gmail.com>
2025-04-29 15:54:45 +0700

    tạo thread

--- khoa/main.c
+++ khoa/main.c
@@ -1,46 +1,27 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <Windows.h>
+#include <string.h>
 
-typedef union
+void printf_log(char* s)
 {
-	int data;
-	struct byte
+	for (int i = 0; i < strlen(s); i++)
 	{
-		char LSB;
-		char byte_1;
-		char byte_2;
-		unsigned char MSB;
-	};
-} wav_t;
+		printf("%c", s[i]);
+		Sleep(1000);
+	}
+}
+
+DWORD WINAPI myThread(LPVOID lpParameter)
+{
+	printf_log("Hello World!\n");
+}
 
 int main()
 {
-	FILE* fptr = fopen("D:/workspace/C/imic/practices/audio.wav", "r");
-	
-	if (fptr == NULL)
-	{
-		printf("Khong the mo file!");
-		return 1; 
-	}
 
-	char str[44];
-	fgets(str, 44, fptr);
+	HANDLE thread = CreateThread(NULL, 0, myThread, NULL, 0, NULL);
 
-	wav_t kichthuoc = { 0 };
-	kichthuoc.LSB = str[4];
-	kichthuoc.byte_1 = str[5];
-	kichthuoc.byte_2 = str[6];
-	kichthuoc.MSB = str[7];
-
-	wav_t rate = { 0 };
-	rate.LSB = str[24];
-	rate.byte_1 = str[25];
-	rate.byte_2 = str[26];
-	rate.MSB = str[27];
-
-	printf("Kich thuoc cua file: %d bytes\n", kichthuoc.data);
-	printf("Audio sample rate cua file: %d kHz\n", rate.data);
-
-	fclose(fptr);
+	printf_log("This is function main!\n");
 	return 0;
 }
