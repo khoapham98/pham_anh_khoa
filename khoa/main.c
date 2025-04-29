3ed6d9e2252eaf667fbd161ec52c28d765f6ff68
anhkh04 <phamanhkhoa9803@gmail.com>
2025-04-29 16:05:50 +0700

    mutex lock

--- khoa/main.c
+++ khoa/main.c
@@ -3,25 +3,37 @@
 #include <Windows.h>
 #include <string.h>
 
+HANDLE mutex_lock;
+char cnt;
+
 void printf_log(char* s)
 {
 	for (int i = 0; i < strlen(s); i++)
 	{
 		printf("%c", s[i]);
-		Sleep(1000);
+		Sleep(300);
 	}
 }
 
 DWORD WINAPI myThread(LPVOID lpParameter)
 {
-	printf_log("Hello World!\n");
+	while (1)
+	{
+		WaitForSingleObject(mutex_lock, INFINITE);
+		printf_log("This is myThread function!\n");
+		ReleaseMutex(mutex_lock);
+	}
 }
 
 int main()
 {
-
 	HANDLE thread = CreateThread(NULL, 0, myThread, NULL, 0, NULL);
-
-	printf_log("This is function main!\n");
+	mutex_lock = CreateMutexA(NULL, 0, NULL);
+	while (1)
+	{
+		WaitForSingleObject(mutex_lock, INFINITE);
+		printf_log("This is main function!\n");
+		ReleaseMutex(mutex_lock);
+	}
 	return 0;
 }
