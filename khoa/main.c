246c7e138c5dbc5cbd3a0e9ae14c48c4eb1cae0f
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-07 22:48:24 +0700

    chỉ thị #if

--- khoa/main.c
+++ khoa/main.c
@@ -3,45 +3,22 @@
 #include <Windows.h>
 #include <string.h>
 
-#include <stdio.h>
-#include <string.h> 
-#include <stdlib.h>
+#define a 4
+#define b 5
 
-
-char* convertToTitle(int columnNumber) {
-    char ch[] = { 0 , 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-                 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-                 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-
-    int num = columnNumber;
-
-    char* str = (char*)calloc(sizeof(char), 10);
-
-    int i = 9;
-    int time = 26;
-    while (num > 0)
-    {
-        int du = num % 26;
-        if (du == 0)
-        {
-            du = 26; 
-            num = num / 26 - 1; 
-        }
-        else
-        {
-            num /= 26;
-        }
-        str[--i] = ch[du];
-    }
-
-    str += i;
-    return str;
+int print(char* s)
+{
+    printf("%s", s); 
 }
+
 int main()
 {
-    char* res = convertToTitle(702);
-
-    printf("%s\n", res);
-
+#if a < b
+    print("a nho hon b");
+#elif a > b
+    print("a lon hon b");
+#else
+    print("a bang b");
+#endif
     return 0;
 }
