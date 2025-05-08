fa167e500e69de53fdbcb57da6434197e44a7e59
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-08 20:48:38 +0700

    đổi sang .cpp

---
+++ khoa/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include "hinh_hoc.h"
+
+int main()
+{
+    phan_so a = { 5, 3 };
+    phan_so b = { 4, 2 };
+
+    phan_so kq = a * b;
+    printf("%d/%d * ", a.tu, a.mau);
+    printf("%d/%d = ", b.tu, b.mau);
+    printf("%d/%d\n", kq.tu, kq.mau);
+
+    kq = a + b;
+    printf("%d/%d + ", a.tu, a.mau);
+    printf("%d/%d = ", b.tu, b.mau);
+    printf("%d/%d\n", kq.tu, kq.mau);
+    return 0;
+}
