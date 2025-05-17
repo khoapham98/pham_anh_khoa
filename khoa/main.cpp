e208116ca3986e2eae37521c4ba21f1f951100fb
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-17 16:50:18 +0700

    thêm class miêu tả đối tượng học sinh

--- khoa/main.cpp
+++ khoa/main.cpp
@@ -1,11 +1,21 @@
 ﻿#include <iostream>
 #include <string>
+#include <stdio.h>
 using namespace std;
 
 enum gioitinh_e
 {
 	NU, NAM
 };
+
+enum xeploai_e
+{
+	GIOI, 
+	KHA, 
+	TRUNGBINH, 
+	YEU
+};
+string xeploai[4] = { "Gioi", "Kha", "Trung binh", "Yeu" };
 
 class ThongTinCaNhan
 {
@@ -24,10 +34,64 @@ public:
 	int get_gioi_tinh() { return this->gioitinh; }
 };
 
+class HocSinh : public ThongTinCaNhan
+{
+private: 
+	float diemtoan; 
+	float diemvan; 
 
+public:
+	void set_diem_toan(float diemtoan) { this->diemtoan = diemtoan; }
+	void set_diem_van(float diemvan) { this->diemvan = diemvan; }
+
+	float get_diem_toan() { return this->diemtoan; }
+	float get_diem_van() { return this->diemvan; }
+
+	float get_dtb()
+	{
+		return (this->diemtoan + this->diemvan) / 2; 
+	}
+
+	xeploai_e get_xep_loai()
+	{
+		float res = get_dtb();
+		if (res > 8)
+		{
+			return GIOI; 
+		}
+		else if (res >= 6.5)
+		{
+			return KHA;
+		}
+		else if (res >= 5)
+		{
+			return TRUNGBINH;
+		}
+		else
+		{
+			return YEU;
+		}
+	}
+};
 
 int main()
 {
+	HocSinh A; 
+	A.set_diem_toan(6); 
+	A.set_diem_van(8);
 
+	cout << xeploai[A.get_xep_loai()] << endl;
 	return 0; 
 }
+
+
+
+
+
+
+
+
+
+
+
+
