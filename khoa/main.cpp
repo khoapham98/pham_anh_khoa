dead64eb78b773005571acb5cae846b5e89ced04
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-22 15:48:37 +0700

    IMIC Test - Bài 1: Viết hàm có đối số là số nguyên 16 bit và sử dụng hàm printf in ra màn hình 2 số gồm 8 bit cao và 8 bit thấp

--- khoa/main.cpp
+++ khoa/main.cpp
@@ -1,119 +1,15 @@
-﻿#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+﻿#include <stdio.h>
 
-using namespace std;
-
-enum gioitinh_e
+void printByteVal(short val)
 {
-	NU, NAM
-};
-
-class ThongTinCaNhan
-{
-private:
-	string ten;
-	int tuoi;
-	gioitinh_e gioitinh; 
-
-public: 
-	void set_ten(string ten) { this->ten = ten; }
-	void set_tuoi(int tuoi) { this->tuoi = tuoi; }
-	void set_gioi_tinh(gioitinh_e gioitinh) { this->gioitinh = gioitinh; }
-
-	string get_ten() { return this->ten; }
-	int get_tuoi() { return this->tuoi; }
-	string get_gioi_tinh() { return gioitinh == NAM ? "Nam" : "Nu"; }
-};
-
-class HocSinh : public ThongTinCaNhan
-{
-private: 
-	float diemtoan; 
-	float diemvan; 
-
-public:
-	HocSinh(string ten, int tuoi, gioitinh_e gt, float diemtoan, float diemvan)
-	{
-		this->set_ten(ten);
-		this->set_tuoi(tuoi);
-		this->set_gioi_tinh(gt);
-		this->diemtoan = diemtoan;
-		this->diemvan = diemvan;
-	}
-
-	void set_diem_toan(float diemtoan) { this->diemtoan = diemtoan; }
-	void set_diem_van(float diemvan) { this->diemvan = diemvan; }
-
-	float get_diem_toan() { return this->diemtoan; }
-	float get_diem_van() { return this->diemvan; }
-	float get_dtb() { return (this->diemtoan + this->diemvan) / 2; }
-	string get_xep_loai()
-	{
-		float res = get_dtb();
-		if (res > 8)
-		{
-			return "Gioi";
-		}
-		else if (res >= 6.5)
-		{
-			return "Kha";
-		}
-		else if (res >= 5)
-		{
-			return "Trung binh";
-		}
-		else
-		{
-			return "Yeu";
-		}
-	}
-};
-
-class GiaoVien : public ThongTinCaNhan
-{
-private: 
-	float bac_luong;
-public: 
-	void set_bac_luong(float bac_luong) { this->bac_luong = bac_luong; }
-	float get_bac_luong() { return this->bac_luong; }
-};
+	unsigned char* ptr = (unsigned char*)&val;
+	printf("High byte: \nDec = %d, Hex = 0x%02X\n", *(ptr + 1), *(ptr + 1));
+	printf("Low byte: \nDec = %d, Hex = 0x%02X\n", *ptr, *ptr);
+}
 
 int main()
 {
-	vector<HocSinh> dshs = {
-		{"Pham Anh Khoa", 21, NAM, 8, 9},
-		{"Tran Quoc Qui", 21, NAM, 7, 8},
-		{"Tommy", 21, NAM, 6, 7} };
-
-
-	cout << "Truoc khi sap xep: " << endl;
-	for (int i = 0; i < 3; i++)
-	{
-		cout << i + 1 << ". " << dshs[i].get_ten() << "," << dshs[i].get_tuoi() << "," << dshs[i].get_gioi_tinh() << "," << dshs[i].get_dtb() << endl;
-	}
-
-	sort(dshs.begin(), dshs.end(), [](HocSinh a, HocSinh b) {
-		if (a.get_dtb() < b.get_dtb())
-			return true;
-		else
-			return false; });
-
-	cout << "Sau khi sap xep: " << endl;
-	for (int i = 0; i < 3; i++)
-	{
-		cout << i + 1 << ". " << dshs[i].get_ten() << "," << dshs[i].get_tuoi() << "," << dshs[i].get_gioi_tinh() << "," << dshs[i].get_dtb() << endl;
-	}
+	printByteVal(1234); 
 
 	return 0; 
 }
-
-
-
-
-
-
-
-
-
